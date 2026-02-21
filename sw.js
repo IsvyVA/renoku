0868c97fa394ed9cff33527d485c075733f938ae
IsvyVA <isvy.araujo@anthesisgroup.com>
2026-02-21 19:55:02 +0100

    Making renoku PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "renoku-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/renoku.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "./fonts/comfortaa.woff2"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
